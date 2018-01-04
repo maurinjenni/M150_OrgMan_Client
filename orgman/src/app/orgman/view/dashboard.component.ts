1c2a5fa848d4c2098480f9bf4d2059776c36caad
BBZW-Sursee Jenni Maurin <Maurin_Jenni@sluz.ch>
2018-01-04 15:32:18 +0100

    dashboard design

--- orgman/src/app/orgman/view/dashboard.component.ts
+++ orgman/src/app/orgman/view/dashboard.component.ts
@@ -3,9 +3,14 @@ import {SelectItem} from 'primeng/primeng';
 import {BreadcrumbService} from '../../breadcrumb.service';
 
 @Component({
-    templateUrl: './dashboard.component.html'
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+    searchResult: any;
+
+    events: any;
 
     constructor(private breadcrumbService: BreadcrumbService) {
       this.breadcrumbService.setItems([
@@ -13,6 +18,26 @@ export class DashboardComponent implements OnInit {
       ]); }
 
     ngOnInit() {
+        this.searchResult = [
+            {'brand': 'VW', 'year': 2012, 'color': 'White', 'vin': 'dsad231ff'},
+            {'brand': 'Audi', 'year': 2011, 'color': 'Black', 'vin': 'gwregre345'},
+            {'brand': 'Renault', 'year': 2005, 'color': 'Gray', 'vin': 'h354htr'},
+            {'brand': 'BMW', 'year': 2003, 'color': 'Blue', 'vin': 'j6w54qgh'},
+            {'brand': 'Mercedes', 'year': 1995, 'color': 'White', 'vin': 'hrtwy34'},
+            ];
+
+        this.events = [
+          '12.12.12: Test',
+          '12.12.12: Test',
+          '12.12.12: Test',
+          '12.12.12: Test',
+          '12.12.12: Test',
+          '12.12.12: Test',
+          '12.12.12: Test',
+          '12.12.12: Test',
+          '12.12.12: Test',
+          '12.12.12: Test'
+        ];
 
     }
 }
