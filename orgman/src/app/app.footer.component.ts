8fd1b20491d84c4fcc56ea59f9753c733334edaa
BBZW-Sursee Jenni Maurin <Maurin_Jenni@sluz.ch>
2018-01-04 15:32:31 +0100

    changed footer

--- orgman/src/app/app.footer.component.ts
+++ orgman/src/app/app.footer.component.ts
@@ -5,7 +5,7 @@ import {Component} from '@angular/core';
     template: `
         <div class="footer">
             <div class="card clearfix">
-                <span class="footer-text-left">PrimeNG ULTIMA for Angular</span>
+                <span class="footer-text-left">OrgMan - Organisation Management</span>
                 <span class="footer-text-right">
                     <span class="material-icons ui-icon-copyright"></span>
                     <span>All Rights Reserved</span>
